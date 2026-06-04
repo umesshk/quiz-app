da55889d75745b60489a1d7f8e34ac28f8d9a168
umesshk <umeshkumar01439@gmail.com>
2026-06-04 12:57:00 +0530

    reading from csv

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello Dear!!! ")
+
+	file, err := os.Open("assets/problems.csv")
+
+	if err != nil {
+		fmt.Println("Error Occured ", err)
+	}
+
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+
+	for {
+		record, err := reader.Read()
+
+		if err != nil {
+			break
+		}
+
+		fmt.Println(record)
+	}
+
 }
